dde8f353f57ee463296bcc4242aff1149b80889a
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-05-05 21:49:03 +0330

    finalize encoder.h

--- lib/encoder.h
+++ lib/encoder.h
@@ -1,6 +1,22 @@
 #ifndef ENCODER
 #define ENCODER
 
+#include <array>
+#include <string>
 
+class encoder {
+	private:
+		static std::map<std::string, std::array<int, 3>> _InstructionHolder;
+	public:
+		static void add_configure(const std::string&, const int, const int, const int);
+		static void add_configure(const std::string&, const int, const int);
+		static void add_configure(const std::string%, const int);
+		void encode_rtype(const std::string&, const int, const int, const int);
+		void encode_itype(const std::string&, const int, const int, const int);
+		void encode_stype(const std::string&, const int, const int, const int);
+		void encode_btype(const std::string&, const int, const int, const int);
+		void encode_utype(const std::string&, const int, const int);
+		void encode_jtype(const std::string&, const int);
+};
 
 #endif
