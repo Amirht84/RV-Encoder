12085f4b2c3add763d0ebe263332e099d03fed33
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-05-06 12:30:15 +0330

    update encoder.cpp

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -5,13 +5,6 @@ static void encoder::add_configure(const std::string& inst, const int op, const 
 	_InstructionHolder.insert({inst, {op, func3, func7}});
 }
 
-template<int bits>
-std::string encoder::get_stringed_bit(const int Num){
-	std::bitset<bits> B(Num);
-	return B.to_string();
-
-}
-
 void encoder::encode_rtype(const std::string& inst, const int rd, const int rs1, const int rs2){
 	auto RD  = std::bitset<5>(rd).to_string();
 	auto RS1 = std::bitset<5>(rs1).to_string();
@@ -22,7 +15,19 @@ void encoder::encode_rtype(const std::string& inst, const int rd, const int rs1,
 	auto F7 = std::bitset<7>(CodeArray[2]).to_string();
 
 	std::cout << F7 + RS2 + RS1 + F3 + RD + OP << '\n';
+}
+void encoder::encode_itype(const std::string& inst, const int rd, const int rs, const int imm12b){
+	auto RD = std::bitset<5>(rd).to_string();
+	auto RS = std::bitset<5>(rs).to_string();
+	auto IMM12B = std::bitset<12>(imm12b).to_string();
+	auto CodeArray = InstructionHolder[inst];
+	auto& OP = std::bitset<7>(CodeArray[0]).to_string();
+	auto& F3 = std::bitset<3>(CodeArray[1]).to_string();
+
 
 }
-
+void encoder::encode_stype(const std::string& inst, const int, const int, const int);
+void encoder::encode_btype(const std::string& inst, const int, const int, const int);
+void encoder::encode_utype(const std::string& inst, const int, const int);
+void encoder::encode_jtype(const std::string& inst, const int);
 
