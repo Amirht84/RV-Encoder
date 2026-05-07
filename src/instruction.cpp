036f1908d1e2dc5e7bdd6771f800a31f0998d446
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-05-07 13:18:08 +0330

    change instruction.cpp to debug instructionFactory

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -2,26 +2,32 @@
 #include "encoder.h"
 
 void rtype::encode(encoder& Encoder){
-	Encoder.encode_rtype(_inst, _rd, _rs1, _rs2);
+	std::cout << _inst << " " << _rd << " " << _rs1 << " " << _rs2 '\n';
+//	Encoder.encode_rtype(_inst, _rd, _rs1, _rs2);
 	return;
 }
 void itype::encode(encoder& Encoder){
-	Encoder.encode_itype(_inst, _rd, _rs, _imm12b);
+	std::cout << _inst << " " << _rd << " " << _rs << " " << _imm12b << '\n';
+//	Encoder.encode_itype(_inst, _rd, _rs, _imm12b);
 	return;
 }
 void stype::encode(encoder& Encoder){
-	Encoder.encode_stype(_inst, _rs1, _rs2, _imm12b); 
+	std::cout << _inst << " " << _rs1 << " " << _rs2 << " " << _imm12b << '\n';
+//	Encoder.encode_stype(_inst, _rs1, _rs2, _imm12b); 
 	return;
 }
 void btype::encode(encoder& Encoder){
-	Encoder.encode_btype(_inst, _rs1, _rs2, _imm12b);
+	std::cout << _inst << " " << _rs1 << " " << _rs2 << " " << _imm12b << '\n';
+//	Encoder.encode_btype(_inst, _rs1, _rs2, _imm12b);
 	return;
 }
 void utype::encode(encoder& Encoder){
-	Encoder.encode_utype(_inst, _rd, _imm20b);
+	std::cout << _inst << " " << _rd << " " << _imm20b << '\n';
+//	Encoder.encode_utype(_inst, _rd, _imm20b);
 	return;
 }
 void jtype::encode(encoder& Encoder){
-	Encoder.encode_jtype(_inst, _rd, _imm20b);
+	std::cout << _inst << " " << _rd << " " << _imm20b << '\n';
+//	Encoder.encode_jtype(_inst, _rd, _imm20b);
 	return;
 }
